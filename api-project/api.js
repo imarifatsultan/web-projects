266faaa0d0e5932e6e01317b2aca319c00b3a806
imarifatsultan <imarifatsultan@gmail.com>
2025-02-20 01:33:11 +0500

    JS file updated

--- api-project/api.js
+++ api-project/api.js
@@ -1,4 +1,5 @@
-const apiKey = "9ef0ab4af56de880182d04b8f340d976"; // Replace with your OpenWeatherMap API key
+// this is web app is simple demonstration of of how API's work
+const apiKey = "9ef0ab4af56de880182d04b8f340d976"; 
 
 async function getWeather() {
     const city = document.getElementById("city").value;
@@ -25,4 +26,5 @@ async function getWeather() {
     } catch (error) {
         document.getElementById("weather").innerText = "Failed to fetch weather data.";
     }
+    
 }
